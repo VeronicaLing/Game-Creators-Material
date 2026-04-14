54b3ac90a43cd7d488ba181ca62f4dd4c5c8f38f
Verônica Ling <ve.ling@outlook.com>
2026-04-14 20:31:00 -0300

    Add files via upload

--- TESTE API/teste-produtos.js
+++ TESTE API/teste-produtos.js
@@ -1,20 +1,45 @@
 const $container = document.getElementById("container")
-const $b1 = document.getElementById("b1")
-const $b2 = document.getElementById("b2")
-const $b3 = document.getAnimations("b3")
-const $b4 = document.getElementById("b4")
-const $b5 = document.getElementById("b5")
-const $b6 = document.getElementById("b6")
+const $container2 = document.getElementById("container2")
+const button1 = document.getElementById("b1")
+const button2 = document.getElementById("b2")
+const button3 = document.getElementById("b3")
+const button4 = document.getElementById("b4")
+const button5 = document.getElementById("b5")
+const button6 = document.getElementById("b6")
 const URL_PRODUTOS = "https://pacoca-store.onrender.com/api/produtos"
-const URL_CATEGORIAS = `https://pacoca-store.onrender.com/api/produtos?categoria=${categoria}`
 
-async function produtos_categorias() {
+async function produtos_categorias(categoria) {
 
-    produtos = await (fetch(URL_PRODUTOS))
-    categorias = await (produtos.json())
+    const url = `${URL_PRODUTOS}?categoria=${categoria}`
+
+    const produtos = await (fetch(url))
+    const categorias = await (produtos.json())
     console.log(produtos)
     console.log(categorias)
-    console.log(categorias[0].url)
+
+    $container2.innerHTML = ""
+
+    categorias.forEach(produto => {
+        const p = document.createElement("div")
+        p.classList.add("b")
+        $container.appendChild(p)
+
+        const produto_info = document.createElement("div")
+        produto_info.classList.add("b1")
+        $container2.appendChild(produto_info)
+
+        const nome_produto = document.createElement("h3")
+        nome_produto.classList.add("produto-nome")
+        nome_produto.textContent = produto.nome
+        produto_info.appendChild(nome_produto)
+
+        const imagem = document.createElement("img")
+        $container2.appendChild(imagem)
+        imagem.classList.add("p")
+        imagem.src = "https://placehold.co/400"
+    })
+
+
 
     console.log(categorias)
     categorias.forEach(() => {
@@ -26,10 +51,6 @@ async function produtos_categorias() {
         let $roupas = document.createElement("p")
         let $livros = document.createElement("p")
         let $diversos = document.createElement("p")
-
-        
-        $all_cat.textContent = categorias[0].categoria
-        $
 
         $container.appendChild($all_cat)
         $container.appendChild($eletronicos)
@@ -45,9 +66,36 @@ async function produtos_categorias() {
         console.log($livros)
         console.log($diversos)
 
+
     })
 
-    
 }
 
+
+button1.addEventListener("click", () => {
+    produtos_categorias("")
+})
+
+button2.addEventListener("click", () => {
+    produtos_categorias("eletronicos")
+})
+
+button3.addEventListener("click", () => {
+    produtos_categorias("eletrodomesticos")
+})
+
+button4.addEventListener("click", () => {
+    produtos_categorias("roupas")
+})
+
+button5.addEventListener("click", () => {
+    produtos_categorias("livros")
+})
+
+button6.addEventListener("click", () => {
+    produtos_categorias("diversos")
+})
+
+
 produtos_categorias()
+
